df5a4109679e6f90b491e20c025349e374473f18
FernandoRS99 <frsnine@gmail.com>
2026-04-04 15:13:03 -0300

    Refresh viewer pages snapshot

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.VIEWER_DEMO_DATA = {
-  "generatedAt": "2026-04-04T18:03:02.517Z",
+  "generatedAt": "2026-04-04T18:13:02.791Z",
   "referenceName": "Renato",
   "dayHours": 9.5,
   "technicians": [
